37d8fc9f5fbd14152d3640d28aa1177804a6f83f
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2026-08-08 22:53:23 +0300

    REMVED

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <h1 className="text-5xl font-bold text-white">
-        Tailwind CSS is working! 🎉
+        Tailwind CSS is workingS! 🎉
       </h1>
     </div>
   );
